51e937b632e6b7d99abbbc47b78ac753c50a8ac4
PiggyDr <84087335+PiggyDr@users.noreply.github.com>
2025-03-20 12:16:51 -0500

    fix pillar

--- src/main/java/com/piggydragons/prismarineenhanced/datagen/PEModelProvider.java
+++ src/main/java/com/piggydragons/prismarineenhanced/datagen/PEModelProvider.java
@@ -16,14 +16,23 @@ public class PEModelProvider extends BlockModelProvider {
     protected void registerModels() {
         cubeAll("cut_prismarine", texLoc("cut_prismarine"));
         cubeBottomTop("reinforced_prismarine", texLoc("reinforced_prismarine_side"), texLoc("reinforced_prismarine_bottom"), texLoc("reinforced_prismarine_top"));
-        cubeColumn("prismarine_pillar", texLoc("prismarine_pillar_side"), texLoc("prismarine_pillar_end"));
-        cubeColumnHorizontal("prismarine_pillar_horizontal", texLoc("prismarine_pillar_side"), texLoc("prismarine_pillar_end"));
+
+        cubeColumnConsistentHorizontal("prismarine_pillar", texLoc("prismarine_pillar_side"), texLoc("prismarine_pillar_end"));
+        cubeColumnConsistentHorizontal("prismarine_pillar_horizontal", texLoc("prismarine_pillar_side"), texLoc("prismarine_pillar_end"));
 
         coralinePrismarine("brain");
         coralinePrismarine("bubble");
         coralinePrismarine("fire");
         coralinePrismarine("horn");
         coralinePrismarine("tube");
+    }
+
+    private void cubeColumnConsistent(String name, ResourceLocation side, ResourceLocation end) {
+        withExistingParent(name, texLoc("cube_column_consistent")).texture("side", side).texture("end", end);
+    }
+
+    private void cubeColumnConsistentHorizontal(String name, ResourceLocation side, ResourceLocation end) {
+        withExistingParent(name, texLoc("cube_column_consistent_horizontal")).texture("side", side).texture("end", end);
     }
 
     private void coralinePrismarine(String coralType) {
